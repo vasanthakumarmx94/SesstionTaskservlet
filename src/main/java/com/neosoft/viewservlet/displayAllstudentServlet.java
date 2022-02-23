b3fe8fb033ddbc94ede8fc32567c7849fe4b627f
vasanthakumarmx94 <vasanthakumarmx94@gmail.com>
2022-02-23 11:17:36 +0530

    submitted on Feb 23 2022

--- src/main/java/com/neosoft/viewservlet/displayAllstudentServlet.java
+++ src/main/java/com/neosoft/viewservlet/displayAllstudentServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import com.google.gson.Gson;
 import com.neosoft.controller.StudentController;
 import com.neosoft.model.Student;
 @WebServlet(urlPatterns = "/displayall")
@@ -36,20 +36,28 @@ public class displayAllstudentServlet extends HttpServlet {
 		
 
 		List<Student> list=StudentController.getAllStudents();
+		 Gson json = new Gson();
+		 
+			
+			List<Student> lst=StudentController.getAllStudents();
+			
+			String responsejsonstrin=json.toJson(lst);
+			out.println(responsejsonstrin);
+			
 	
-		out.println("<table class='table table-bordered table-striped'>");
-		out.println("<tr><th>StudentId</th><th>Student Name</th><th>Course</th><th>Marks</th></th><th colspan='2'>Action</th></tr>");
-		for (Student bean : list) {
-			out.println("<tr><td>" + bean.getStid() + "</td><td>" + bean.getStname() + " </td><td>" + bean.getStcourse()
-					+ "</td><td> " + bean.getMarks() + "</td><td><a href='EditEmployeeForm?id=" + bean.getStid()
-					+ "'><button class='btn btn-primary'> Edit</button></a></td><td><a href='DeleteEmployee?id=" + bean.getStid() + "'><button class='btn btn-danger'> Delete</button></a></td></tr>");
-		}
-		out.println("</table>");
-		
-		out.println("<h2><a href='studentdashboard.html'><button>Got Home</button></a></h2>");
-		out.println("</body>");
-		
-		out.print("</html>");
+//		out.println("<table class='table table-bordered table-striped'>");
+//		out.println("<tr><th>StudentId</th><th>Student Name</th><th>Course</th><th>Marks</th></th><th colspan='2'>Action</th></tr>");
+//		for (Student bean : list) {
+//			out.println("<tr><td>" + bean.getStid() + "</td><td>" + bean.getStname() + " </td><td>" + bean.getStcourse()
+//					+ "</td><td> " + bean.getMarks() + "</td><td><a href='EditEmployeeForm?id=" + bean.getStid()
+//					+ "'><button class='btn btn-primary'> Edit</button></a></td><td><a href='DeleteEmployee?id=" + bean.getStid() + "'><button class='btn btn-danger'> Delete</button></a></td></tr>");
+//		}
+//		out.println("</table>");
+//		
+//		out.println("<h2><a href='studentdashboard.html'><button>Got Home</button></a></h2>");
+//		out.println("</body>");
+//		
+//		out.print("</html>");
 		
 		out.close();
 	}
